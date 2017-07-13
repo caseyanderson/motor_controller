5a02d07a786c7250c9f424df98989ce8bcbf6220
caseyanderson <casey.thomas.anderson@gmail.com>
2017-07-13 12:17:34 -0700

    software is done

--- motor_controller/motor_controller.ino
+++ motor_controller/motor_controller.ino
@@ -7,7 +7,6 @@
 
 static const uint8_t analog_pins[] = {A0, A1, A2, A3};
 static const uint8_t digital_pins[] = {3, 5, 6, 9};
-int val[] = {0, 0, 0, 0};
 
 void setup() {
   Serial.begin(9600);
@@ -21,8 +20,8 @@ void setup() {
 
 void loop() {
   for (int i = 0; i < 4; i++) {
-    val[i] = analogRead(analog_pins[i]);
-    analogWrite(digital_pins[i], map( val[i], 0, 1023, 0, 255 ));
+    unsigned int val = analogRead(analog_pins[i]);
+    analogWrite(digital_pins[i], val / 4);
   }
   delay(2);
 }
